77b9e0149c27756f1d5d0ee49e4df251c618b421
EIKE Oliveira <101583887+eikehacker1@users.noreply.github.com>
2023-03-11 07:06:49 -0300

    Add files via upload

---
+++ recon.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+while getopts ":d:" opt; do
+  case ${opt} in
+    d )
+      domain=$OPTARG
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    : )
+      echo "Option -$OPTARG requires an argument" >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo "$domain" | assetfinder | httprobe | waybackurls | tee -a mass.txt
+
+wait
+
+cat mass.txt | gf xss | tee -a xss.txt
+cat mass.txt | gf redirect | tee -a redirect.txt
+cat mass.txt | gf rce | tee -a rce
+cat mass.txt | gf sqli | tee -a sqli
+cat mass.txt | gf ssti | tee -a ssti
+cat mass.txt | gf idor | tee -a idor
