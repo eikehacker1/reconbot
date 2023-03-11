e63a2a8837226bdbfcdfec75d7ae78ad02469952
EIKE Oliveira <101583887+eikehacker1@users.noreply.github.com>
2023-03-11 07:06:27 -0300

    Add files via upload

---
+++ instal.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+sudo apt install -y golang
+
+
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/tomnomnom/assetfinder@latest
+go install github.com/tomnomnom/httprobe@latest
+go install github.com/ferreiraklet/airixss@latest
+go install github.com/tomnomnom/qsreplace@latest
+go install github.com/tomnomnom/anew@latest
+
+wait
+
+
+cp /root/go/bin/* /usr/bin
+
+
+git clone https://github.com/tomnomnom/gf
+git clone https://github.com/1ndianl33t/Gf-Patterns
+
+
+mkdir ~/.gf
+cp -r gf/examples/* ~/.gf
+cp -r Gf-Patterns/* ~/.gf
